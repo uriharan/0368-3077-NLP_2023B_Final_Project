a3a4823d64783e60632081d59f8ca89b6920369d
Uri Haran <uri.haran@gmail.com>
2023-08-12 16:06:29 +0300

    edit setenv

--- setenv.py
+++ setenv.py
@@ -3,4 +3,6 @@ import os
 if __name__ == "__main__":
     curr_dict = os.getcwd()
     os.environ["TORCH_HOME"] = curr_dict + "/code/results/cache/xdg"
+    print("TORCH_HOME = " + os.environ['TORCH_HOME'])
     os.environ["XDG_CACHE_HOME"] = curr_dict + "/code/results/cache/torch"
+    print("XDG_CACHE_HOME = " + os.environ['XDG_CACHE_HOME'])
