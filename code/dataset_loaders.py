03d87a02087fa9ecba99d632ca0ff396628cd6dc
Uri Haran <uri.haran@gmail.com>
2023-09-09 19:02:30 +0300

    added prints

--- code/dataset_loaders.py
+++ code/dataset_loaders.py
@@ -23,9 +23,11 @@ def Load_Dataset(dataset_path, local_path, dataset_name, do_unzip):
 # If (set_num/4)%2 = 1, uses the test dataset.
 # Datasets are appended together. set_num = 7 results in the combination of all datasets.
 def Load_MathQA(set_num,do_load):
+    print("Fetching MathQA contents")
     assert (isinstance(set_num, int) and (set_num % 8 != 0)), "Load_MathQA received invalid set_num! {}".format(set_num)
     
     if do_load:
+        print("Loading MathQA to local")
         Load_Dataset(MATH_QA_DATASET_PATH, LOCAL_FOLDER + MATH_QA_LOCAL, MATH_QA_DATASET_NAME, True)
     
     usecols = ["Problem","options","correct"]
@@ -49,9 +51,11 @@ def Load_MathQA(set_num,do_load):
         for i in range(len(test_qa['Problem'])):
             math_qa_vec.append((test_qa['Problem'][i],test_qa['options'][i],test_qa['correct'][i]))
 
+    print("MathQA lines fetched: {}".format(len(math_qa_vec)))
     return math_qa_vec
 
 if __name__ == "main":
+    print("entered dataset_loaders as main")
     dataset = Load_MathQA(4,True)
     for row in dataset[0:min(len(dataset),10)]:
         print(row)
